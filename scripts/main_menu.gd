bd082a528c4c80196897c1aafd3ede447ebb55ad
SirWadles <adrian2hoa@outlook.com>
2025-08-24 02:20:02 -0700

    So Much New Stuff
    Organized the files. Added a main menu with a start, options, and quit button. also some music. only start and quit work options just exist. each play a sound when pressed. Fixed the wheels from looking like a fish's tail when turning. Drift and Boost have their particles working now. Developed the car's model a bit more. Plan on making working break lights and possibly headlight if I want to make a level in the dark at some point. You can also see backwards. I think that's it.

---
+++ scripts/main_menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+@onready var start_button = $CenterContainer/VBoxContainer/StartButton
+@onready var options_button = $CenterContainer/VBoxContainer/OptionsButton
+@onready var quit_button = $CenterContainer/VBoxContainer/QuitButton
+
+@onready var button_sound_1 = $ButtonSound_1
+@onready var button_sound_2 = $ButtonSound_2
+@onready var music_player = $MusicPlayer
+
+
+func _ready():
+	start_button.pressed.connect(_on_start_button_pressed)
+	options_button.pressed.connect(_on_options_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+	start_button.grab_focus()
+	if not music_player.playing:
+		music_player.play()
+
+func _on_start_button_pressed():
+	print("Starting game...")
+	button_sound_1.play()
+	await button_sound_1.finished
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_options_button_pressed():
+	print("Options Button Pressed")
+	button_sound_1.play()
+	await button_sound_1.finished
+
+func _on_quit_button_pressed():
+	print("Quiting game...")
+	button_sound_2.play()
+	await button_sound_2.finished
+	get_tree().quit()
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		_on_start_button_pressed()
+	if event.is_action_pressed("ui_cancel"):
+		_on_quit_button_pressed()
